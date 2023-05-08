449af44b07a487a5bcb558d9aa427808a88e05a1
Lewistrick <erickwilts@gmail.com>
2023-05-08 15:26:52 +0200

    Fix division getter route

--- src/routes/exact.py
+++ src/routes/exact.py
@@ -1,16 +1,20 @@
 import requests
+import xmltodict
 from fastapi import APIRouter, Depends
 from fastapi.exceptions import HTTPException
+from loguru import logger
 
 from src.config import settings
-from src.routes.api import get_token_dict
+from src.routes.api import get_auth_headers
 
-router = APIRouter(prefix="/api/exact", tags=["Exact endpoints"])
+router = APIRouter(prefix="/api/exact", tags=["Exact"])
 
 
 @router.get("/division")
-async def division(token: str = Depends(get_token_dict)):
-    div_url = settings.exact_apiurl + "/Me?$select=CurrentDivision"
+async def division(auth_headers: str = Depends(get_auth_headers)) -> int:
+    logger.debug("Requesting division")
+    div_url = settings.exact_apiurl + "/Me?$select=UserID,CurrentDivision,UserName"
+    token = auth_headers["access_token"]
     headers = {"authorization": f"Bearer {token}"}
 
     resp = requests.get(div_url, headers=headers)
@@ -18,4 +22,16 @@ async def division(token: str = Depends(get_token_dict)):
     if not resp.ok:
         raise HTTPException(status_code=resp.status_code, detail=resp.content)
 
-    return resp.json()
+    parsed_resp = xmltodict.parse(resp.text)
+    division_number = (
+        parsed_resp.get("feed")
+        .get("entry")
+        .get("content")
+        .get("m:properties")
+        .get("d:CurrentDivision")
+        .get("#text")
+    )
+
+    div_id = int(division_number)
+
+    return div_id
